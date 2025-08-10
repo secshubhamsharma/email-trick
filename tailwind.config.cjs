5c1e6ccf2a27c48411a2b83cc847d23692c4017e
secshubhamsharma <secshubhamsharma@gmail.com>
2025-08-10 23:52:48 +0530

    initial project

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+// tailwind.config.cjs
+module.exports = {
+    content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+    theme: { extend: {} },
+    plugins: [require('daisyui')],
+    daisyui: {
+      themes: ['light','dark'],
+    },
+  }
